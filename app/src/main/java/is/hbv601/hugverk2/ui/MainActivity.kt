095815fd611e9b068ed2600b6b74c7cbcbe4668f
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2025-02-15 22:02:55 +0000

    login and registration

--- app/src/main/java/is/hbv601/hugverk2/ui/MainActivity.kt
+++ app/src/main/java/is/hbv601/hugverk2/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package `is`.hbv601.hugverk2.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Navigate to LoginActivity when the app starts
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish() // Close MainActivity to prevent going back to it
+
         setContent {
             Hugverk2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
---
+++ app/src/main/java/is/hbv601/hugverk2/ui/RecipientHomeActivity.kt
@@ -0,0 +1,13 @@
+package `is`.hbv601.hugverk2.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import `is`.hbv601.hugverk2.R
+
+class RecipientHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipient_home)
+    }
+}
