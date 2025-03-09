dcd524d4efdae6b07dbb8e2635a944f16f912b73
thrandurorriolason <thrandurorriolason@gmail.com>
2025-03-09 15:58:09 +0000

    cookies

--- app/src/main/java/is/hbv601/hugverk2/data/api/RetrofitClient.kt
+++ app/src/main/java/is/hbv601/hugverk2/data/api/RetrofitClient.kt
@@ -12,10 +12,10 @@ import java.net.CookiePolicy
 import okhttp3.JavaNetCookieJar
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.101.4:8080/"
+    private const val BASE_URL = "http://130.208.102.88:8080/"
 
     // Create a CookieManager that accepts all cookies.
-    private val cookieManager = CookieManager().apply {
+    public val cookieManager = CookieManager().apply {
         setCookiePolicy(CookiePolicy.ACCEPT_ALL)
     }
 
