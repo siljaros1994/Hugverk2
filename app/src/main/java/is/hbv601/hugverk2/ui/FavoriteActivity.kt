1a9e7be31a86f0ed7d346cad1e74886a191cd800
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2025-03-30 20:39:38 +0000

    logout

--- app/src/main/java/is/hbv601/hugverk2/ui/FavoriteActivity.kt
+++ app/src/main/java/is/hbv601/hugverk2/ui/FavoriteActivity.kt
@@ -160,22 +160,6 @@ class FavoriteActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
             R.id.nav_booking -> {
                 Toast.makeText(this, "Booking clicked", Toast.LENGTH_SHORT).show()
             }
-            /*
-            R.id.nav_logout -> { //Matches navigation menu ID
-                Log.d("RecipientHomeActivity", "Logout button clicked!") //Debugging Log
-                //Close the navigation drawer before logging out
-                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                }
-                //Delay logout slightly to prevent UI conflicts
-                drawerLayout.postDelayed({
-                    val intent = Intent(this, LogoutActivity::class.java)
-                    startActivity(intent) //Call logout function
-                    finish()
-                }, 300) //Small delay ensures smooth UI transition
-            }
-
-             */
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
