e41e25c0ffc0e79dfec2387777a1cc1cd3724242
Guðrún Ísabella Kjartansdóttir <gik10@hi.is>
2025-04-09 10:06:35 +0000

    finishing touches, adding appropriate titles for pages

--- app/src/main/java/is/hbv601/hugverk2/data/api/RetrofitClient.kt
+++ app/src/main/java/is/hbv601/hugverk2/data/api/RetrofitClient.kt
@@ -12,7 +12,9 @@ import okhttp3.JavaNetCookieJar
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.101.4:8080"
+    private const val BASE_URL = "http://192.168.0.100:8080"
+
+
 
     // Create a CookieManager that accepts all cookies.
     private val cookieManager = CookieManager().apply {
